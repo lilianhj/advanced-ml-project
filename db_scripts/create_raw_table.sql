c61adf2263e40731e2bb8f33bc2f6910841fbf22
lilianhj <lilianhj@users.noreply.github.com>
2020-04-22 23:33:44 -0500

    Update create_raw_table.sql

--- db_scripts/create_raw_table.sql
+++ db_scripts/create_raw_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE raw_data (dab_id VARCHAR(7) PRIMARY KEY,
                        alj_text TEXT,
                        dab_url VARCHAR(255),
                        alj_url VARCHAR(255),
-                       decision INTEGER);
+                       decision TEXT);
 CREATE INDEX ON raw_data (dab_id);
 CREATE INDEX ON raw_data (alj_id);
