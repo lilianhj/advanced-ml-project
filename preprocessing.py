c5768509b778eeb5f0d4c6e25d7ee3c0d835112d
Katy Koenig <kkoenig810@gmail.com>
2020-06-03 22:53:52 -0500

    this is a mess but pushing for backup

---
+++ preprocessing.py
@@ -0,0 +1,133 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import spacy
+import torch
+import torchtext
+from torchtext.data import Field, TabularDataset, Dataset
+import nltk
+import scipy
+import psycopg2 as ps
+import dill
+
+
+nlp = spacy.load("en")
+nlp.max_length = 20000000
+
+
+def connect_db():
+    '''
+    '''
+    host_name = 'adv-ml-db.ben-fogarty.com'
+    dbname = 'postgres'
+    port = '5432'
+    user_name = 'readerwriter'
+    pwd = '35ZH@q&hajeU'
+
+    try:
+        conn = ps.connect(host=host_name, database=dbname,
+                        user=user_name, password=pwd, port=port)
+    except ps.OperationalError as e:
+        raise e
+    else:
+        print('Connected!')
+    cur = conn.cursor()
+    cur.execute(""" 
+                SELECT * FROM raw_data; 
+                """)
+    data = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description] 
+    conn.commit()
+
+    #create the pandas dataframe
+    df = pd.DataFrame(data)
+    df.columns = colnames
+
+    #close the connection
+    cur.close()
+    return df
+
+
+def light_clean(df):
+    '''
+    '''
+    sans_nulls_df = df[(df['alj_text'].notnull()) & (df['decision_binary'].notnull())]
+    sans_nulls_df['alj_text'] = sans_nulls_df['alj_text']. \
+                                    apply(lambda x: x[0: 1000000])
+    return sans_nulls_df
+
+
+def get_split_write(train_csv, test_csv, val_csv):
+    '''
+    '''
+    # get data
+    # clean it
+    # split it by years
+    # write it out
+    
+
+
+def word_tokenize(text):
+    tokenized = []
+    # pass word list through language model.
+    doc = nlp(text)
+    for token in doc:
+        if not token.is_punct and len(token.text.strip()) > 0:
+            tokenized.append(token.text)
+    return tokenized
+
+def normalize_tokens(word_list, extra_stop=[]):
+    #We can use a generator here as we just need to iterate over it
+    normalized = []
+    if type(word_list) == list and len(word_list) == 1:
+        word_list = word_list[0]
+
+    if type(word_list) == list:
+        word_list = ' '.join([str(elem) for elem in word_list]) 
+    doc = nlp(word_list.lower())
+    # add the property of stop word to words considered as stop words
+    if len(extra_stop) > 0:
+        for stopword in extra_stop:
+            lexeme = nlp.vocab[stopword]
+            lexeme.is_stop = True
+    for w in doc:
+        # if it's not a stop word or punctuation mark, add it to our article
+        if w.text != '\n' and not w.is_stop and not w.is_punct \
+            and not w.like_num and len(w.text.strip()) > 0:
+            # we add the lematized version of the word
+            normalized.append(str(w.lemma_))
+    return normalized
+
+
+def make_dataset(train_csv, val_csv):
+    '''
+    '''
+    TEXT = Field(sequential=True, tokenize=word_tokenize,
+                                 preprocessing=normalize_tokens)
+    LABEL = Field(sequential=False)
+    data_fields = [('alj_text', TEXT), ('decision_binary', LABEL)]
+    train, val = TabularDataset.splits(path='', train=train_csv, validation=val_csv,
+                                       format='csv', fields=data_fields, skip_header=True)
+    return train, val, TEXT, LABEL
+
+
+def other_shit(train, val, TEXT):
+    TEXT.build_vocab(train, val)
+    return TEXT
+
+# from this 
+# https://stackoverflow.com/questions/53421999/how-to-save-torchtext-dataset
+def save_dataset(dataset, path):
+    if not isinstance(path, Path):
+        path = Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+    torch.save(dataset.examples, path/"examples.pkl", pickle_module=dill)
+    torch.save(dataset.fields, path/"fields.pkl", pickle_module=dill)
+
+
+def load_dataset(path):
+    if not isinstance(path, Path):
+        path = Path(path)
+    examples = torch.load(path/"examples.pkl", pickle_module=dill)
+    fields = torch.load(path/"fields.pkl", pickle_module=dill)
+    return Dataset(examples, fields)
