71a002119d06ed2244765858c4627b4c799c7d27
Ben Fogarty <ben.t.fogarty@gmail.com>
2020-06-08 13:00:56 -0500

    lint and docstring training.py

--- modeling/training.py
+++ modeling/training.py
@@ -1,12 +1,15 @@
+'''
+Classes and functions common to model training, evaluation, and selection
+
+June 2020
+'''
+
 from collections import namedtuple
 import copy
-import random
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-from torchtext import data
 
 ModelResults = namedtuple('ModelResults',
                           ['model', 'accuracy', 'precision', 'recall'])
@@ -14,39 +17,60 @@ ModelResults = namedtuple('ModelResults',
 
 def prediction_stats(preds, y):
     '''
-    Return true/false positives/negatives per patch
+    Return all the values in a confusion matrix for a set of predictions and
+    their labels. Only for use with binary prediction tasks.
+
+    Inputs:
+    preds (torch.tensor): predicted scores not yet converted to binary
+        predictions
+    y (torch.tensor): true labels for each of examples being predicted
+
+    Returns: 4-ple of floats, (number of true positives, number of false
+        positives, number of true negatives, number of false negatives)
     '''
     rounded_preds = torch.round(torch.sigmoid(preds))
     true_pos = ((rounded_preds == y) & (rounded_preds == 1)).sum()
     false_pos = ((rounded_preds != y) & (rounded_preds == 1)).sum()
     true_neg = ((rounded_preds == y) & (rounded_preds == 0)).sum()
     false_neg = ((rounded_preds != y) & (rounded_preds == 0)).sum()
-    
+
     return true_pos.item(), false_pos.item(), true_neg.item(), false_neg.item()
 
 class TrainingModule():
+    '''
+    Class for training and evaluating PyTorch models.
+    '''
 
     def __init__(self, model, lr, pos_weight, use_cuda, num_epochs):
+        '''
+        Initialize a TrainingModule.
+
+        Inputs:
+        model (torch.nn.Module): the PyTorch model to be used
+        lr (float): learning rate for training
+        pos_weight (float): weight to assign to positive-labeled examples during
+            training
+        use_cuda (bool): whether or not to use a GPU during traing/evaluation
+        num_epochs (int): number of epochs to run when training
+        '''
         self.model = model
         self.use_cuda = use_cuda
         if self.use_cuda:
             model = model.cuda()
         self.epochs = num_epochs
-       
+
         ##YOUR CODE HERE##
         # Choose an optimizer. optim.Adam is a popular choice
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
         self.loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-    
+
     def train_epoch(self, iterator):
         '''
-        Train the model for one epoch. For this repeat the following, 
-        going through all training examples.
-        1. Get the next batch of inputs from the iterator.
-        2. Determine the predictions using a forward pass.
-        3. Compute the loss.
-        4. Compute gradients using a backward pass.
-        5. Execute one step of the optimizer to update the model paramters.
+        Train the model for one epoch.
+
+        Inputs:
+        iterator (iterable): iterator over batches to train the model with in
+        the epoch
         '''
         epoch_loss = 0
         epoch_tp = 0
@@ -54,13 +78,12 @@ class TrainingModule():
         epoch_tn = 0
         epoch_fn = 0
         self.model.train()
-    
+
         for batch in iterator:
-          # batch.text has the texts and batch.label has the labels.
-        
+          # batch.alj_text has the texts and batch.decision_binary has the labels.
+
             self.optimizer.zero_grad()
-                
-            ##YOUR CODE HERE##
+
             text = batch.alj_text
             target = batch.decision_binary
             if self.use_cuda:
@@ -72,28 +95,36 @@ class TrainingModule():
 
             loss.backward()
             self.optimizer.step()
-            
+
             epoch_loss += loss.item()
-            
+
             batch_tp, batch_fp, batch_tn, batch_fn = prediction_stats(predictions, target)
             epoch_tp += batch_tp
             epoch_fp += batch_fp
             epoch_tn += batch_tn
             epoch_fn += batch_fn
-        
+
         loss = epoch_loss / (epoch_tp + epoch_tn + epoch_fp + epoch_fn)
         acc = (epoch_tp + epoch_tn) / (epoch_tp + epoch_tn + epoch_fp + epoch_fn)
-        prec = ((epoch_tp) / (epoch_tp + epoch_fp) if (epoch_tp + epoch_fp) > 0 else float('nan'))
-        rec = ((epoch_tp) / (epoch_tp + epoch_fn) if (epoch_tp + epoch_fn) > 0 else float('nan')) 
-        
+        prec = ((epoch_tp) / (epoch_tp + epoch_fp) if (epoch_tp + epoch_fp) > 0
+                else float('nan'))
+        rec = ((epoch_tp) / (epoch_tp + epoch_fn) if (epoch_tp + epoch_fn) > 0
+               else float('nan'))
+
         return loss, acc, prec, rec
 
-    
+
     def train_model(self, train_iterator, dev_iterator):
         """
         Train the model for multiple epochs, and after each evaluate on the
-        development set.  Return the best performing model.
-        """  
+        development set.  Return the best performing model on the basis of
+        accuracy, precision, and recall.
+
+        Inputs:
+        train_iterator (iterable): iterator over batches to train the model with
+        dev_iterator (iterable): iterator over batches to evaluate model
+        performance and select models with
+        """
         dev_accs = [0.]
         dev_precs = [0.]
         dev_recs = [0.]
@@ -117,10 +148,13 @@ class TrainingModule():
         return {'accuracy': best_model_acc,
                 'precision': best_model_prec,
                 'recall': best_model_rec}
-                
+
     def evaluate(self, iterator):
         '''
         Evaluate the performance of the model on the given examples.
+
+        Inputs:
+        iterator (iterable): iterator over batches to evaluate the model with
         '''
         epoch_loss = 0
         epoch_tp = 0
@@ -128,31 +162,33 @@ class TrainingModule():
         epoch_tn = 0
         epoch_fn = 0
         self.model.eval()
-    
+
         with torch.no_grad():
-    
+
             for batch in iterator:
-                
+
                 text = batch.alj_text
                 target = batch.decision_binary
                 if self.use_cuda:
                     text = text.cuda()
                     target = target.cuda()
-                
+
                 predictions = self.model.forward(text).squeeze()
                 loss = self.loss_fn(predictions, target)
-        
+
                 epoch_loss += loss.item()
-            
+
                 batch_tp, batch_fp, batch_tn, batch_fn = prediction_stats(predictions, target)
                 epoch_tp += batch_tp
                 epoch_fp += batch_fp
                 epoch_tn += batch_tn
                 epoch_fn += batch_fn
-        
+
         loss = epoch_loss / (epoch_tp + epoch_tn + epoch_fp + epoch_fn)
         acc = (epoch_tp + epoch_tn) / (epoch_tp + epoch_tn + epoch_fp + epoch_fn)
-        prec = ((epoch_tp) / (epoch_tp + epoch_fp) if (epoch_tp + epoch_fp) > 0 else float('nan'))
-        rec = ((epoch_tp) / (epoch_tp + epoch_fn) if (epoch_tp + epoch_fn) > 0 else float('nan')) 
-        
+        prec = ((epoch_tp) / (epoch_tp + epoch_fp) if (epoch_tp + epoch_fp) > 0
+                else float('nan'))
+        rec = ((epoch_tp) / (epoch_tp + epoch_fn) if (epoch_tp + epoch_fn) > 0
+               else float('nan'))
+
         return loss, acc, prec, rec
